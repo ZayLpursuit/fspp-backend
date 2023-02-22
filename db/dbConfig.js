4f6c9ed177fbd28f736d03bba5a55c4bf463bb7b
ZayLpursuit <Zaylors@pursuit.org>
2023-02-22 16:26:58 -0500

    fixed config

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,13 +2,17 @@ const pgp=require("pg-promise")()
 
 require("dotenv").config()
 
-const cn={
-    host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  ssl: true
+
+const cn=process.env.DATABASE_URL?{
+    connectionString:process.env.DATABASE_URL,
+    max:30,
 }
+
+:
+{   host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,}
 const db=pgp(cn)
 
 module.exports=db
